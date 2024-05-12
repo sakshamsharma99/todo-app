28da89f6690c18678c3ed058ae2e6cee52c9e9a8
Saksham Sharma <saksham600j@gmail.com>
2024-05-12 15:41:30 +0530

    marked as completed on click

--- frontend/src/Components/Todos.jsx
+++ frontend/src/Components/Todos.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 export function Todos({todos}) {
     return <div>
         {todos.map(function (todo){
             return <div>
                 <h1>{todo.title}</h1>
                 <h2>{todo.description}</h2>
-                <button>{todo.completed == true ? "Completed" : "Mark as Completed"}</button>
+
+                <button onClick={ async ()=>{
+                    await fetch(`http://localhost:3000/completed`, {
+                        method:"PUT",
+                        body: JSON.stringify({ id: todo._id }),
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    })
+                }}>{todo.completed == true ? "Completed" : "Mark as Completed"}</button>
             </div>
         })}
     </div>
